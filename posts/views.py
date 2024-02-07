45c05be9d10de5fe6e8633ecbdfddc753b676099
Oscar Telleria <theomjty@gmail.com>
2024-02-07 11:28:57 -0400

    print text

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from  .models import posts
+from django.views.generic import ListView
 # Create your views here.
+
+
+class home_page_view(ListView):
+    model = posts
+    template_name = "home.html"
